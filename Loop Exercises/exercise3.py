505d6d42608c42484e17bc783bc8b3da2271a535
Zaw Ye Naung <168292949+Tandra18@users.noreply.github.com>
2024-05-30 16:33:15 +0630

    "new changes"

--- Loop Exercises/exercise3.py
+++ Loop Exercises/exercise3.py
@@ -1,1 +1,13 @@
-copyright()
+#Calculate the sum of all numbers from 1 to a given number
+
+#First solution
+# s = 0
+# n = int(input('Enter a number :'))
+# for i in range(1, n+1, 1):
+#     s += i
+# print("The sum is ", s)
+
+#Second solution
+# n = int(input("Enter a number :"))
+# s = sum(range(1,n+1))
+# print(s)
