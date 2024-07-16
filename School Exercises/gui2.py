e8aaa0471f2710c246f010f712914259f4f82abf
zaw ye anung <topgzawyenaung@gmail.com>
2024-07-16 10:21:44 +0630

    shits fixed

--- School Exercises/gui2.py
+++ School Exercises/gui2.py
@@ -4,5 +4,6 @@ top =  tkinter.Tk()
 def helloCallBack():
     messagebox.showinfo("Hello Python", "Hello Whore")
 
-B = tkinter.Button(top, text="Hello", command= helloCallBack())
+B = tkinter.Button(top, text = "Hello", command = helloCallBack())
 B.pack()
+
